fa87f3a228f0413fd7379d7a1dd48d2fae6c9958
NABOO\Thomas <thomasvandenbossche94@gmail.com>
2017-12-18 19:01:47 +0100

    alles weg

--- ExamenJava2017/src/db/DAO.java
+++ ExamenJava2017/src/db/DAO.java
@@ -16,43 +16,8 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class DAO {
 
-	// Connection connection =
-	// DriverManager.getConnection("jdbc:mysql://localhost:3306/afstandsbediening","root","Twdih300");
-		String url = "jdbc:mysql://localhost:3306/afstandsbediening";
-	// String user = "root@localhost";
-	// String password = "Twdih300";
-	// String Query = "Select * from afstandsbediening.bewoners";
 
-	private Properties properties;
-	private Connection connection;
 
-	private Properties GetDBproperties(String filename) throws IOException {
-		properties = new Properties();
-		try {
-			FileInputStream fileInputStream = new FileInputStream(filename);
-			properties.load(fileInputStream);
-			fileInputStream.close();
-		} catch (FileNotFoundException e1) {
-			JOptionPane.showMessageDialog(null, "verbinding niet gelukt");
-			e1.printStackTrace();
-		}
-
-		return properties;
-
-	}
-
-	public Connection ConnectToDB() throws IOException, SQLException {
-		GetDBproperties("DBproperties.properties");
-		if (connection == null) {
-			try {
-				Class.forName(properties.getProperty("driver"));
-			} catch (ClassNotFoundException e) {
-
-				e.printStackTrace();
-			}
-			connection = DriverManager.getConnection(url, properties);
-		}
-		return connection;
-	}
+	
 
 }
